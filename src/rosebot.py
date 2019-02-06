3475dc62d5c7f9bba7c4614dfccc4113c4bcf5da
rogerscj <rogerscj@rose-hulman.edu>
2019-02-06 13:28:29 -0500

    debugging calibrate

--- src/rosebot.py
+++ src/rosebot.py
@@ -210,19 +210,20 @@ class ArmAndClaw(object):
                (i.e., 14.2 motor revolutions),
           3. Resets the motor's position to 0.
         """
-
+        print('begin calibrate')
         self.motor.turn_on(100)
         while True:
             if self.touch_sensor.is_pressed() == True:
                 self.motor.turn_off()
                 break
+        print('end loop 1')
         self.motor.reset_position()
         self.motor.turn_on(-100)
         while True:
-
             if abs(self.motor.get_position()) >= (14.2 * 360) :
                 self.motor.turn_off()
                 break
+        print('end loop 2')
         self.motor.reset_position()
 
 
