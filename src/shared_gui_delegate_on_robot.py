53ebbdfb5379aa3d196c5ca902c238532e0d4ac1
sudj <sudj@rose-hulman.edu>
2019-02-13 13:05:33 -0500

    testing beep_grab method

--- src/shared_gui_delegate_on_robot.py
+++ src/shared_gui_delegate_on_robot.py
@@ -6,10 +6,12 @@
     and Christina Rogers, Jason Ims, Dan Su.
   Winter term, 2018-2019.
 """
+import m1_run_this_on_robot
 
 class DelegateThatReceives(object):
     def __init__(self, robot):
         """:type robot: rosebot.RoseBot"""
+        self.grab = m1_run_this_on_robot.Grab(robot)
         self.robot = robot
         self.quit = False
 
@@ -76,4 +78,8 @@ class DelegateThatReceives(object):
 
     def less_intensity(self, intensity, speed):
         self.robot.drive_system.go_straight_until_intensity_is_less_than(intensity, speed)
+
+    def m1_beep_grab(self):
+        self.grab.beep_grab()
+
 
